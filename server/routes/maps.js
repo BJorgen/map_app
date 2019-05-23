6ef7ef76ee1593146b5a94ea21b681653fd8abb2
venetrius <gergelygjuhasz@gmail.com>
2019-05-23 21:12:11 +0000

    resolve test_GET_maps_Imap_points_Ipoint

--- server/routes/maps.js
+++ server/routes/maps.js
@@ -12,9 +12,14 @@ const getMap = function (req, res){
   res.status(400).send();
 };
 
+const getPoint = function (req, res){
+  res.status(400).send();
+};
+
 module.exports = function(DataHelpers) {
   mapsRoutes.get("/", get);
   mapsRoutes.get("/:map", getMap);
+  mapsRoutes.get("/:map/points/:point", getPoint);
 
   return mapsRoutes;
 }();
