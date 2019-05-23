8702db42d62853b540cc3881bf4abe081fa041ba
Britta Jorgenson <bljorgen@gmail.com>
2019-05-23 16:32:57 +0000

    Install dotenv and set up database - knex init.

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
