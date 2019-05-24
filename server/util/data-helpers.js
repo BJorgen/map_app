f1ea851255c1ad9ef161c364f55c1d6b8f64a7de
Britta Jorgenson <bljorgen@gmail.com>
2019-05-24 04:26:25 +0000

    add data helpers to read map, points and user info from db

---
+++ server/util/data-helpers.js
@@ -0,0 +1,90 @@
+require('dotenv').config();
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
+      ssl      : process.env.DB_SSL
+    }
+});
+
+
+function getMapSettings(mapSettingId, cb) {
+  knex.select('*').from('map_settings')
+    .where('id', mapSettingId)
+    .asCallback(function(err, mapSettings) {
+      if (err) {
+        throw err;
+      }
+      cb(null,mapSettings[0]);
+      });
+}
+
+function getMap(mapId, cb) {
+ let map;
+  knex.select('*').from('maps')
+    .where('id', mapId)
+    .asCallback(function(err, mapData) {
+      if (err) {
+        throw err;
+      }
+      map = mapData[0];
+      console.log(map.map_setting_id)
+      getMapSettings(map.map_setting_id , function(err, res) {
+        if (err) {
+          throw err;
+        }
+        map.settings = res;
+        cb(map)
+      });
+    });
+}
+
+
+function getPointImages(pointId, cb) {
+  knex.select('*').from('images')
+  .where('point_id', pointId)
+  .asCallback(function(err, pointImages) {
+    if (err) {
+      throw err;
+    }
+    cb(pointImages)
+  });
+}
+
+
+function getMapPoints(mapId, cb) {
+  knex.select('*',{ image_id: 'images.id' }).from('points')
+    .innerJoin('images','points.id','images.point_id')
+    .where('map_id', mapId)
+    .asCallback(function(err, mapPoints) {
+      if (err) {
+        throw err;
+      }
+      points = mapPoints;
+      cb(mapPoints)
+    });
+}
+
+function getUserWithId(userId, cb) {
+  knex.select('*').from('users')
+    .where('id', userId)
+    .asCallback(function(err, userInfo) {
+      if (err) {
+        throw err;
+      }
+      cb(userInfo[0])
+    });
+}
+
+
+// --------- Test Function Calls -----
+
+//getMap(3, console.log);
+//getMapPoints(3, console.log)
+//getPointImages(3, console.log)
+//getUserWithId(1, console.log)
---
+++ server/util/mock/mock_data.js
@@ -0,0 +1,46 @@
+module.exports = function(mockData) {
+  
+const map_object = {
+  id : 3,
+  name: 'Public Art Work',
+  public: true,
+  user_id: 2,
+  map_setting: {
+    id: 3,
+    center_long: -114.082400,
+    center_lat: 51.067900,
+    zoom: 12.00
+  }
+}
+
+const points = [ { id: 3,
+  title: 'Travelling Light',
+  description: 'A controversial art piece and a very big circle',
+  created_at: 2019-05-23T22:27:05.414Z,
+  longitude: '-114.042263',
+  latitude: '51.142729',
+  map_id: 3,
+  image_url: 'https://images.thestar.com/1Rz4yOpeL9aEexFVDcagcc4Le9A=/1086x716/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/calgary/2018/06/03/proposed-fixes-to-calgarys-controversial-public-art-program-going-to-council/bluering.jpg',
+  point_id: 4,
+  image_id: 3 },
+{ id: 4,
+  title: 'Wonderland Sculpture',
+  description: 'Big head in the center of town',
+  created_at: 2019-05-23T22:27:05.414Z,
+  longitude: '-114.062122',
+  latitude: '51.047597',
+  map_id: 3,
+  image_url: 'https://325mwx119m59jqt5r27qnkcn-wpengine.netdna-ssl.com/wp-content/uploads/2017/04/PUBLICART-Encana-5-1-1024x790.jpg',
+  point_id: 5,
+  image_id: 4 },
+{ id: 5,
+  title: 'Armengol Statues',
+  description: 'Very tall people to look up to',
+  created_at: 2019-05-23T22:27:05.414Z,
+  longitude: '-114.059937',
+  latitude: '51.047669',
+  map_id: 3,
+  image_url: 'https://static1.squarespace.com/static/50e1b9c6e4b015296ce398f6/t/54e8c652e4b0440df7a30994/1424541305832/public+art+Calgary?format=1000w',
+  point_id: 6,
+  image_id: 5 } ]
+}
