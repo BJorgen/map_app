b72766a77311e3765096887b0bc3aff1d64b7dba
Britta Jorgenson <bljorgen@gmail.com>
2019-05-27 00:51:46 +0000

    toggle and style favourite heart button on map cards

--- public/scripts/map-favourites.js
+++ public/scripts/map-favourites.js
@@ -5,21 +5,51 @@
 //      - listen for favourite/unfavourite
 // ======================================================
 
-function toggleFavourite(map_id){
-  console.log('Class Names : ',$(this))
-  $.ajax({
-    url : '/maps/' + map_id + '/favourite',
-    method: 'POST',
-    success: function(res){
-      console.log(res);
-    },
-    error: function(req, textStatus, errorThrown) {
-      console.log("error", errorThrown);
-      alert("you have left the happy path")
-    }
-  });
+function toggleFavourite(){
+  const heart = $(this);
+  const mapId = $(this.dataset)[0].mapId;
+  const heartClasses = $(this).attr('class').split(/\s+/);
+  console.log('Classes:', heartClasses)
+  console.log('map_id : ', mapId)
+
+  if (heartClasses.includes('favourite')) {
+    
+    $.ajax({
+      url : '/maps/' + mapId + '/favourite',
+      method: 'DELETE',
+      success: function(res){
+        heart.removeClass('favourite')
+        console.log(res);
+      },
+      error: function(req, textStatus, errorThrown) {
+        console.log("error", errorThrown);
+        alert("you have left the happy path")
+      }
+    });
+  
+  } else {
+    
+    $.ajax({
+      url : '/maps/' + mapId + '/favourite',
+      method: 'POST',
+      success: function(res){
+        heart.addClass('favourite')
+        console.log(res);
+      },
+      error: function(req, textStatus, errorThrown) {
+        console.log("error", errorThrown);
+        alert("you have left the happy path")
+      }
+    });
+  }
+
 
 }
+
+$(document).ready(function() {
+  $('.map_favourites .fa-heart').on('click', toggleFavourite)
+});
+
 
 
 // mapsRoutes.post("/:map/favorite", addMapFavourite);
