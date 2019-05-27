cfc4b3c472ac153c8db7e2cc096d3d19db282dd7
venetrius <gergelygjuhasz@gmail.com>
2019-05-27 21:55:04 +0000

    code review with bribri

--- public/scripts/map-favourites.js
+++ public/scripts/map-favourites.js
@@ -1,47 +1,25 @@
-// ======================================================
-//                 map-favourites.js
-// ======================================================
-// On page load:
-//      - listen for favourite/unfavourite
-// ======================================================
-
 function toggleFavourite(){
+  // retrive components and data from DOM
   const heart = $(this);
   const mapId = $(this.dataset)[0].mapId;
   const heartClasses = $(this).attr('class').split(/\s+/);
-  console.log('Classes:', heartClasses)
-  console.log('map_id : ', mapId)
 
-  if (heartClasses.includes('favourite')) {
-    
-    $.ajax({
-      url : '/maps/' + mapId + '/favourite',
-      method: 'DELETE',
-      success: function(res){
-        heart.removeClass('favourite')
-        console.log(res.length);
-      },
-      error: function(req, textStatus, errorThrown) {
-        console.log("error", errorThrown);
-        alert("you have left the happy path")
-      }
-    });
-  
-  } else {
-    
-    $.ajax({
-      url : '/maps/' + mapId + '/favourite',
-      method: 'POST',
-      success: function(res){
-        heart.addClass('favourite')
-        console.log(res.length);
-      },
-      error: function(req, textStatus, errorThrown) {
-        console.log("error", errorThrown);
-        alert("you have left the happy path")
-      }
-    });
-  }
+  const isFavourite = heartClasses.includes('favourite');
+  const removeClass = a => heart.removeClass('favourite');
+  const addClass = a => heart.addClass('favourite'); 
+  // AJAX call can be POST or DELETE depending on current state (liked or not)
+  const method = isFavourite ? "DELETE" : "POST";
+  const onSuccess = isFavourite ? removeClass : addClass;
+  // sending AJAX
+  $.ajax({
+    url : '/maps/' + mapId + '/favourite',
+    method: method,
+    success: onSuccess,
+    error: function(req, textStatus, errorThrown) {
+      console.log("error", errorThrown);
+      alert("you have left the happy path")
+    }
+  });
 }
 
 
